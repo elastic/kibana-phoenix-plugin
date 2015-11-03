810eae2c79d381ed7c38591954ec10c4a833383a
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-11-02 16:31:40 -0800

    updating phx version, adding documentation to index.html, changing chart controller to account for changes in the phx api

--- public/lib/chart/ChartCtrl.js
+++ public/lib/chart/ChartCtrl.js
@@ -3,7 +3,7 @@ var module = require('ui/modules').get('apps/phoenix', []);
 module.controller('ChartCtrl', function ($scope) {
   var faker = require('plugins/phoenix/lib/fakeData.js');
   var options = require('plugins/phoenix/lib/options.json');
-  var data = [{ data: [faker()] }];
+  var data = { data: faker() };
 
   $scope.ui = {
     data: JSON.stringify(data, null, 2),
