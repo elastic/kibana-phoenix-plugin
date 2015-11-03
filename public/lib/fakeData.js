8b6d4c859ee701587543ebf46979e72405116cfe
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-11-03 00:22:20 -0800

    adding makeString function to fakeData to create pie chart data, adding more documentation to index.html

--- public/lib/fakeData.js
+++ public/lib/fakeData.js
@@ -1,10 +1,22 @@
 var _ = require('lodash');
 var defaultOptions = {
   size: 50,
+  type: 'series',
   startTime: (new Date()).getTime(), // in ms format
   label: 'Label 1',
   interval: 1000 * 30 // 30 seconds
 };
+
+function makeString() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i=0; i < 5; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+}
 
 function fakeData(pref) {
   var settings = _.assign({}, defaultOptions, pref);
@@ -13,10 +25,17 @@ function fakeData(pref) {
   var count = 0;
 
   while(count < settings.size) {
-    data.push({
-      x: settings.startTime + count * settings.interval,
-      y: Math.floor(Math.random() * 100)
-    });
+    if (settings.type === 'series') {
+      data.push({
+        x: settings.startTime + count * settings.interval,
+        y: Math.floor(Math.random() * 100)
+      });
+    } else {
+      data.push({
+        x: makeString(),
+        y: Math.floor(Math.random() * 100)
+      });
+    }
     ++count;
   }
 
